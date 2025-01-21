447226be5a1f98279b18673a63f7c311a5886b99
Arturstriker3 <artur.danielgama@gmail.com>
2025-01-21 17:06:40 -0300

    ✨ feat: Enhance main application setup with Swagger documentation and global validation pipes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API documentation for Notoriun Service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Listening on port ${port}`);
 }
+
 bootstrap();
