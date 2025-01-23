31c7710cc8836110d0d803bd3bf831c84fd38c2b
Arturstriker3 <artur.danielgama@gmail.com>
2025-01-23 13:52:12 -0300

    ✨ feat: enhance RegisterView2 component; improve form layout and adjust styling

--- client/src/components/RegisterView2.vue
+++ client/src/components/RegisterView2.vue
@@ -5,55 +5,59 @@
       style="min-height: 402px;"
     >
       <v-card-text>
-        <v-form class="mt-4">
-          <div class="grid grid-cols-2 gap-4">
-            <div>
-              <label class="text-subtitle-1">
-                Número do CNPJ*
-              </label>
+        <v-form class="mt-4 mx-4">
+          <v-row
+            class="mb-4"
+            dense
+          >
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Número do CNPJ*</label>
               <v-text-field
                 placeholder="Digite o número de CNPJ"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Nome*
-              </label>
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Nome*</label>
               <v-text-field
                 placeholder="Digite o nome da Instituição"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Telefone contato
-              </label>
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Telefone contato</label>
               <v-text-field
                 placeholder="(00) 00000-0000"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Email da instituição
-              </label>
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Email da instituição</label>
               <v-text-field
                 placeholder="instituicao@financeira.com.br"
                 density="compact"
                 variant="solo"
               />
-            </div>
-          </div>
+            </v-col>
+          </v-row>
 
           <div class="border-t-2 border-black mb-8" />
 
@@ -61,91 +65,99 @@
             Digite o CEP e complete as informações
           </p>
 
-          <div class="grid grid-cols-2 gap-4">
-            <div>
-              <label class="text-subtitle-1">
-                CEP*
-              </label>
+          <v-row
+            class="mb-4"
+            dense
+          >
+            <v-col
+              cols="12"
+              md="4"
+            >
+              <label class="text-subtitle-1">CEP*</label>
               <v-text-field
                 placeholder="Digite o CEP da localidade"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Estado*
-              </label>
+            <v-col
+              cols="12"
+              md="4"
+            >
+              <label class="text-subtitle-1">Estado*</label>
               <v-text-field
                 placeholder="Estado"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Cidade*
-              </label>
+            <v-col
+              cols="12"
+              md="4"
+            >
+              <label class="text-subtitle-1">Cidade*</label>
               <v-text-field
                 placeholder="Cidade"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
+          </v-row>
 
-            <div>
-              <label class="text-subtitle-1">
-                Bairro*
-              </label>
+          <v-row
+            class="mb-4"
+            dense
+          >
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Bairro*</label>
               <v-text-field
                 placeholder="Exemplo: Centro"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Endereço*
-              </label>
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Endereço*</label>
               <v-text-field
                 placeholder="Exemplo: Avenida Brasil"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Número*
-              </label>
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Número*</label>
               <v-text-field
                 placeholder="Exemplo: 123"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
+            </v-col>
 
-            <div>
-              <label class="text-subtitle-1">
-                Complemento*
-              </label>
+            <v-col
+              cols="12"
+              md="6"
+            >
+              <label class="text-subtitle-1">Complemento*</label>
               <v-text-field
                 placeholder="Exemplo: Sala 01"
-                class="mb-4"
                 density="compact"
                 variant="solo"
               />
-            </div>
-          </div>
+            </v-col>
+          </v-row>
         </v-form>
       </v-card-text>
     </v-card>
