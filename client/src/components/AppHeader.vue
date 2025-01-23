577900f87726005b9641ac55abb380dcd4bea886
Arturstriker3 <artur.danielgama@gmail.com>
2025-01-23 11:08:06 -0300

    ✨ feat: add navigation functionality to AppHeader; create users page

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+
+const router = useRouter();
+
+const goTo = (route: string) => {
+  router.push(`/${route}`);
+};
+
+</script>
+
 <template>
   <div class="flex flex-col sticky top-0 z-10 bg-white">
     <header class="flex items-center w-full h-20 px-12 shadow-md">
@@ -8,10 +18,16 @@
       </div>
 
       <div class="flex items-center space-x-4 w-1/2 justify-end">
-        <v-btn icon>
+        <v-btn
+          icon
+          @click="goTo('')"
+        >
           <v-icon>mdi-account-plus</v-icon>
         </v-btn>
-        <v-btn icon>
+        <v-btn
+          icon
+          @click="goTo('users')"
+        >
           <v-icon>mdi-account-group</v-icon>
         </v-btn>
       </div>
---
+++ client/src/pages/users.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+// const atualView = ref(1);
+
+</script>
+
+<template>
+  <h2>
+    Users View
+  </h2>
+</template>
